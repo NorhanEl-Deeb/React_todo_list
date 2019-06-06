715881bbdcf904bd49e873418af1bcfe9f3f058a
norhangit <norhan@yahoo.com>
2019-06-06 02:14:41 +0200

    Updates

---
+++ precache-manifest.775047826474cf057d907b59c021b12a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e70d47e46a0bf59749de6c9d314e2b98",
+    "url": "/React_todo_list/index.html"
+  },
+  {
+    "revision": "4d646ebb66c511b4a7a2",
+    "url": "/React_todo_list/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7938608d938bdfed8612",
+    "url": "/React_todo_list/static/css/main.0c86b5dd.chunk.css"
+  },
+  {
+    "revision": "4d646ebb66c511b4a7a2",
+    "url": "/React_todo_list/static/js/2.1d31daf8.chunk.js"
+  },
+  {
+    "revision": "7938608d938bdfed8612",
+    "url": "/React_todo_list/static/js/main.cae0b7e3.chunk.js"
+  },
+  {
+    "revision": "1f1e475c118b03c30afb",
+    "url": "/React_todo_list/static/js/runtime~main.f87b505e.js"
+  }
+]);
